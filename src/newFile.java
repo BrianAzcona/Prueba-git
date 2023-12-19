4aee10c8fe3fc5f72e07f6fd7af4255ee3e5dd48
BrianAzcona <145722223+BrianAzcona@users.noreply.github.com>
2023-12-19 16:58:12 -0300

    cambio desde repositorio remoto

--- src/newFile.java
+++ src/newFile.java
@@ -2,5 +2,6 @@ public class newFile {
     public static void main(String[] args) {
         System.out.println("2do hola mundo");
         System.out.println("1er cambio");
+        System.out.println("cambio desde repositorio remoto");
     }
 }
