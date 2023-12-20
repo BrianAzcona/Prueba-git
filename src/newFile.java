dd3226d5aebc4a80696970502094c4d233d48cc5
BrianAzcona <brianazcona45@gmail.com>
2023-12-20 18:01:49 -0300

    hellow master

--- src/newFile.java
+++ src/newFile.java
@@ -1,7 +1,7 @@
 public class newFile {
     public static void main(String[] args) {
         System.out.println("2do hola mundo");
-        System.out.println("1er cambio");
+        System.out.println("hellow master");
         System.out.println("cambio desde repositorio remoto");
         System.out.println("Saludo desde nueva rama");
         System.out.println("se unificaron los cambios");
